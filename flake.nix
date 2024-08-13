168cc6eda25ddde258a2f3527d8c4cd79ea0fa39
urob <978080+urob@users.noreply.github.com>
2024-08-13 16:04:48 -0400

    Add keymap-drawer workflow
    I haven't tuned the output. For now this is just a demonstration of the
    workflow.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    # Pin to 23.11 to provide python38 dependency for sdk
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    # Pin this to 23.11 to provide py3.8 needed for the sdk-ng without building it ourselves
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     # Zephyr version determining which requirements.txt to use to install python dependencies
     zephyr.url = "github:zephyrproject-rtos/zephyr/v3.5.0";
@@ -9,24 +9,40 @@
 
     # Zephyr sdk and host tools
     zephyr-nix.url = "github:urob/zephyr-nix";
-    zephyr-nix.inputs.nixpkgs.follows = "nixpkgs";
+    # zephyr-nix.inputs.nixpkgs.follows = "nixpkgs";
     zephyr-nix.inputs.zephyr.follows = "zephyr";
   };
 
-  outputs = { self, nixpkgs, zephyr-nix, ... }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    zephyr = zephyr-nix.packages.${system};
+  outputs = { nixpkgs, zephyr-nix, ... }: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
   in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        zephyr.hosttools
-        zephyr.pythonEnv
-        zephyr.sdkFull
-        pkgs.cmake
-        pkgs.dtc
-        pkgs.ninja
-      ];
-    };
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      zephyr = zephyr-nix.packages.${system};
+      keymap_drawer = pkgs.python3Packages.callPackage ./draw { };
+
+    in {
+      default = pkgs.mkShell {
+        packages = [
+          keymap_drawer
+
+          zephyr.hosttools-nix
+          zephyr.pythonEnv
+          (zephyr.sdk.override { targets = [ "arm-zephyr-eabi" ]; })
+
+          pkgs.cmake
+          pkgs.ninja
+
+          # Uncomment these if you don't have system-wide versions:
+          # pkgs.gawk             # awk
+          # pkgs.unixtools.column # column
+          # pkgs.coreutils        # cp, cut, echo, mkdir, sort, tail, uniq etc.
+          # pkgs.gnugrep          # grep
+          pkgs.just               # just
+          # pkgs.gnused           # sed
+          pkgs.yq                 # yq
+        ];
+      };
+    });
   };
 }
